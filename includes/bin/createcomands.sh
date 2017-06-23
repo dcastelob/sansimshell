8f8b2365f74ba961d3e3dcaf54fcba7a52405c4e
Diego Castelo Branco <dcastelob@gmail.com>
2017-06-23 18:54:01 -0300

    primeiro commit

---
+++ includes/bin/createcomands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#create the simbolics links
+
+#rm alicopy alicreate alidelete alirename alishow cfgcopy cfgcreate cfgdelete cfgdisable cfgenable cfgrename cfgshow zonecopy zonecreate zonedelete zonerename zoneshow
+
+ln -s aliascmd alicopy
+ln -s aliascmd alicreate
+ln -s aliascmd alidelete
+ln -s aliascmd alirename
+ln -s aliascmd alishow
+
+ln -s cfgcmd cfgcopy
+ln -s cfgcmd cfgcreate
+ln -s cfgcmd cfgdelete
+ln -s cfgcmd cfgdisable
+ln -s cfgcmd cfgenable
+ln -s cfgcmd cfgrename
+ln -s cfgcmd cfgshow
+ln -s cfgcmd cfgsave
+
+ln -s zonecmd zonecopy
+ln -s zonecmd zonecreate
+ln -s zonecmd zonedelete
+ln -s zonecmd zonerename
+ln -s zonecmd zoneshow
+
+ln -s defzonecmd defzone
